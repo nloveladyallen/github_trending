b7a289feb77bc6c7b5d9bc80540c2da97fb016cb
Noah Lovelady-Allen <email@private>
2016-04-15 21:16:48 -0400

    initial scraper commit

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,39 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env ruby
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'nokogiri'
+require 'json'
+require 'open-uri'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+class Repo
+  def initialize(full_name)
+    @name = %r{/(\w+)}.match(full_name)[1]
+    @api_url = 'https://api.github.com/repos/' + full_name
+    # to use API authentication, modify the request here
+    rate_limited = JSON.parse(open('https://api.github.com/rate_limit').read)['resources']['core']['remaining'] == 0
+    info unless rate_limited
+  end
+
+  def info
+    # to use API authentication, modify the request here
+    @api_hash = JSON.parse(open(@api_url).read)
+    @api_arr = []
+    @api_hash.each do |p|
+      @api_arr.push(p[1])
+    end
+  end
+
+  attr_reader :name, :api_hash, :api_arr
+end
+
+trending_page_html = Nokogiri::HTML(open('https://github.com/trending'))
+repo_html_list = trending_page_html.css('.repo-list-name a')
+repo_name_list = repo_html_list.map do |f|
+  f.content.delete("\n").delete(' ')
+end
+repo_object_list = repo_name_list.map do |f|
+  Repo.new(f)
+end
+
+repo_object_list.each do |f|
+  puts f.name
+end
